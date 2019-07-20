8c18c49dd447c0ca7c539b64dc8ea889b69ca18b
AwEi <chenwei791129@livemail.tw>
2019-07-20 15:46:37 +0800

    add debug info

--- setup-v2ray.sh
+++ setup-v2ray.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 if [[ ${DENY_LAN_ACCESS} ]]; then
+  echo 'apply DENY_LAN_ACCESS'
   cp -f /tmp/config-local-security.json /etc/v2ray/config.json
 fi
 
 if [[ -n "${PORT}" ]]; then
+  echo 'setup port.'
   echo $(cat /etc/v2ray/config.json | jq '.inbounds[0].port = '${PORT}'') > /etc/v2ray/config.json
 fi
 
 if [[ -n "${ID}" ]]; then
+  echo 'setup id.'
   echo $(cat /etc/v2ray/config.json | jq '.inbounds[0].settings.clients[0].id = "'${ID}'"') > /etc/v2ray/config.json
 fi
 
 if [[ -n "${ALTERID}" ]]; then
+  echo 'setup alterId.'
   echo $(cat /etc/v2ray/config.json | jq '.inbounds[0].settings.clients[0].alterId = '${ALTERID}'') > /etc/v2ray/config.json
 fi
